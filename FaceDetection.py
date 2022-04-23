d19495be14e76f6732fa2a4e37e360c065b3ed57
shadaashraf <60737095+shadaashraf@users.noreply.github.com>
2022-04-23 05:17:45 +0200

    Add files via upload

---
+++ FaceDetection.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+import cv2,os
+
+data_path=r'C:\Users\GCC 2\Downloads\dataset\New folder'
+categories=os.listdir(data_path)
+labels=[i for i in range(len(categories))]
+
+label_dict=dict(zip(categories,labels))
+print(label_dict)
+print(categories)
+print(labels)
+
+
+# In[13]:
+
+
+img_size=100
+data=[]
+target=[]
+
+
+for category in categories:
+    folder_path=os.path.join(data_path,category)
+    img_names=os.listdir(folder_path)
+        
+    for img_name in img_names:
+        img_path=os.path.join(folder_path,img_name)
+        img=cv2.imread(img_path)
+
+        try:
+            gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)           
+            #Coverting the image into gray scale
+            resized=cv2.resize(gray,(img_size,img_size))
+            #resizing the gray scale into 100x100, since we need a fixed common size for all the images in the dataset
+            data.append(resized)
+            target.append(label_dict[category])
+            #appending the image and the label(categorized) into the list (dataset)
+
+        except Exception as e:
+            print('Exception:',e)
+            #if any exception rasied, the exception will be printed here. And pass to the next image
+
+
+# In[14]:
+
+
+import numpy as np
+
+data=np.array(data)/255.0
+data=np.reshape(data,(data.shape[0],img_size,img_size,1))
+target=np.array(target)
+
+from keras.utils import np_utils
+
+new_target=np_utils.to_categorical(target)
+
+
+# In[15]:
+
+
+np.save('data',data)
+np.save('target',new_target)
+
+
+# In[16]:
+
+
+import numpy as np
+
+data=np.load('data.npy')
+target=np.load('target.npy')
+
+#loading the save numpy arrays in the previous code
+
+
+# In[17]:
+
+
+from keras.models import Sequential
+from keras.layers import Dense,Activation,Flatten,Dropout
+from keras.layers import Conv2D,MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+
+model=Sequential()
+
+model.add(Conv2D(200,(3,3),input_shape=data.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+#The first CNN layer followed by Relu and MaxPooling layers
+
+model.add(Conv2D(100,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+#The second convolution layer followed by Relu and MaxPooling layers
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+#Flatten layer to stack the output convolutions from second convolution layer
+model.add(Dense(50,activation='relu'))
+#Dense layer of 64 neurons
+model.add(Dense(2,activation='softmax'))
+#The Final layer with two outputs for two categories
+
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+
+# In[18]:
+
+
+from sklearn.model_selection import train_test_split
+
+train_data,test_data,train_target,test_target=train_test_split(data,target,test_size=0.1)
+
+
+# In[19]:
+
+
+checkpoint = ModelCheckpoint('model-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True,mode='auto')
+history=model.fit(train_data,train_target,epochs=20,callbacks=[checkpoint],validation_split=0.2)
+
+
+# In[20]:
+
+
+print(model.evaluate(test_data,test_target))
+
+
+# In[ ]:
+
+
+
+
